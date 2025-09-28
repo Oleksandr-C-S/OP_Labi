db0ba4ef9a9b19a4aeeeae385cccef2685d726ca
Oleksandr-C-S <oleksandrchakun@gmail.com>
2025-09-28 16:08:59 +0200

    Update Laba1.js

--- Laba1/Laba1.js
+++ Laba1/Laba1.js
@@ -1,13 +1,10 @@
 //Скалярні типи та посилання
 //Підготуйте дві реалізації функції inc:
-
 //З сигнатурою inc(n: number): number, приклад виклику: const a = 5; const b = inc(a); console.dir({a, b});
 
 function inc(n) {
   return n + 1;
 }
-
 let a = 5;
 let b = inc(a);
-
 console.log("a =", a, "b =", b);
